4a3262188ea89c95bca317110daa0c96d3beaeb0
kevin <kevin665511111@gmail.com>
2026-09-17 21:17:27 +0800

    feat: readme(#260917)

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # LiveCamera
-相機測試
+
+車載相機客戶端。透過 Spinnaker SDK 讀取相機影像，即時顯示在畫面上，並透過 WebSocket 將 GPS 座標與相機影像串流回後端伺服器（fleetMonitor）。
+
+## 使用技術
+
+- C++17
+- [Spinnaker SDK](https://www.flir.com/products/spinnaker-sdk/)（FLIR 相機控制與影像擷取）
+- OpenCV 4（影像處理與視窗顯示）
+- Qt5（取得螢幕解析度以置中視窗）
+- [websocketpp](https://github.com/zaphoyd/websocketpp)（WebSocket 客戶端，git submodule）
+- libcurl / libicuuc / Mapnik
--- main.cpp
+++ main.cpp
@@ -24,9 +24,9 @@ void setNodeValue(CIntegerPtr nodePtr, int value, const string &nodeName) {
 }
 
 int main(int argc, char **argv) {
-    string vehicleId = "RDJ-1770"; // 車牌號碼
+    string vehicleId = "BUG-0604"; // 車牌號碼
     string token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOiJqdWFodWEiLCJ1c2VybmFtZSI6Imp1YWh1YSIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE3MDQ5NTg1NDB9._LwnFsdTMOKzE0F2mIMiYPb6NJusLNXIzUjvktTHyjY"; // 金鑰
-    string wsUri = "ws://localhost:3002/fleetMonitor?type=post&id=" + vehicleId + "&token=" + token;
+    string wsUri = "ws://211.23.157.201:3002/fleetMonitor?type=post&id=" + vehicleId + "&token=" + token;
 
     int cameraWidth = 2000; // 相機解析度
     int cameraHeight = 1500;
